ffaa1662b4d7d1e584ac3f777d24450d936c6578
Kévin KNITTEL <knittel.kevin@outlook.fr>
2021-04-24 17:50:20 +0200

    .

--- deploy/api/index.php
+++ deploy/api/index.php
@@ -63,6 +63,8 @@
       $token_jwt = JWT::encode($payload,JWT_SECRET, "HS256");
       // $response = $response->withHeader("Authorization", "Bearer {$token_jwt}")->write(json_encode($token_jwt));
 
+      addCorsHeaders($response);
+
       return $response;
   });
 
