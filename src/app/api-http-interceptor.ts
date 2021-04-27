e5d892f56929d0be763b618ad713a7714e3b1f65
Kévin KNITTEL <kevin.knittel@merckel.fr>
2021-04-27 20:04:33 +0200

    .

--- src/app/api-http-interceptor.ts
+++ src/app/api-http-interceptor.ts
@@ -1,18 +1,15 @@
-import { UserService } from './service/user.service';
-import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, of, pipe } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse, HttpErrorResponse } from "@angular/common/http";
+import { Observable, pipe } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { Router } from '@angular/router'
 
 @Injectable()
 export class ApiHttpInterceptor implements HttpInterceptor {
-
   jwtToken: String = "";
-
-  constructor(private userService: UserService) { }
+  constructor(private router: Router) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
     if (this.jwtToken != "") {
       req = req.clone({ setHeaders: { Authorization: `Bearer ${this.jwtToken}` } });
     }
@@ -22,34 +19,80 @@ export class ApiHttpInterceptor implements HttpInterceptor {
         if (evt instanceof HttpResponse) {
           let tab: Array<String>;
           let enteteAuthorization = evt.headers.get("Authorization");
-
           if (enteteAuthorization != null) {
             tab = enteteAuthorization.split(/Bearer\s+(.*)$/i);
-
             if (tab.length > 1) {
               this.jwtToken = tab[1];
             }
           }
         }
+      },
 
-        // (error: HttpErrorResponse) => {
-        //   switch (error.status) {
-        //     case 400:
-        //     case 401:
-        //   }
-
-        //   return of(null);
-        // }
-      }));
+      (error: HttpErrorResponse) => {
+        switch (error.status) {
+          case 401:
+            break;
+          default:
+            console.log("ERROR !!!!!")
+        }
+      }
+    )
+    )
   }
-
-  // intercept(req: HttpRequest<any>, next: HttpHandler) {
-  //   let tokenizedReq = req.clone({
-  //     setHeaders: {
-  //       Authorization: `Bearer ${this.userService.getToken()}`
-  //     }
-  //   })
-
-  //   return next.handle(tokenizedReq)
-  // }
 }
+
+// import { UserService } from './service/user.service';
+// import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { Observable, of, pipe } from 'rxjs';
+// import { tap } from 'rxjs/operators';
+
+// @Injectable()
+// export class ApiHttpInterceptor implements HttpInterceptor {
+
+//   jwtToken: String = "";
+
+//   constructor(private userService: UserService) { }
+
+//   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+//     if (this.jwtToken != "") {
+//       req = req.clone({ setHeaders: { Authorization: `Bearer ${this.jwtToken}` } });
+//     }
+
+//     return next.handle(req).pipe(tap(
+//       (evt: HttpEvent<any>) => {
+//         if (evt instanceof HttpResponse) {
+//           let tab: Array<String>;
+//           let enteteAuthorization = evt.headers.get("Authorization");
+
+//           if (enteteAuthorization != null) {
+//             tab = enteteAuthorization.split(/Bearer\s+(.*)$/i);
+
+//             if (tab.length > 1) {
+//               this.jwtToken = tab[1];
+//             }
+//           }
+//         }
+
+//         // (error: HttpErrorResponse) => {
+//         //   switch (error.status) {
+//         //     case 400:
+//         //     case 401:
+//         //   }
+
+//         //   return of(null);
+//         // }
+//       }));
+//   }
+
+//   // intercept(req: HttpRequest<any>, next: HttpHandler) {
+//   //   let tokenizedReq = req.clone({
+//   //     setHeaders: {
+//   //       Authorization: `Bearer ${this.userService.getToken()}`
+//   //     }
+//   //   })
+
+//   //   return next.handle(tokenizedReq)
+//   // }
+// }
